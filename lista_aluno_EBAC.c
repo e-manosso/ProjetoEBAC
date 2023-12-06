52f1a0d3683d0cb38bb5538800e2876692bb26ce
Erik Luiz Manosso <153079904+e-manosso@users.noreply.github.com>
2023-12-06 08:18:25 -0300

    Repetição de funções
    Adicionada funcionalidade nas funções de criar, consultar e deletar usuário para que o operador do programa escolha se deseja executar novamente a função que está utilizando, antes de retornar ao menu principal.

--- lista_aluno_EBAC.c
+++ lista_aluno_EBAC.c
@@ -11,45 +11,50 @@ int registro()	//Função responsavel pela criação de usuário
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
+	int novo_registro=1;	//variavel para verificar opção de novo registro
 	
-	printf("|||		Registro de Novo Usuário da EBAC		|||\n\n");
-	printf("Digite o CPF para registro: ");
-	scanf("%s",cpf);
+	do
+	{
+		printf("|||		Registro de Novo Usuário da EBAC		|||\n\n");
+		printf("Digite o CPF para registro: ");
+		scanf("%s",cpf);
+			
+		strcpy(arquivo,cpf);
+			
+		FILE *file;
+		file=fopen(arquivo, "w");	//cria arquivo de dados, com o mesmo valor de cpf. "w" para criar, Write
+		fprintf(file,cpf);
+		fprintf(file," , ");
+		fclose(file);
+			
+		printf("\nDigite o Nome para registro: ");
+		scanf("%s",nome);
+			
+		file=fopen(arquivo, "a");	//atualização do arquivo com a variavel nome. "a" para atualizar
+		fprintf(file,nome);
+		fprintf(file," , ");
+		fclose(file);
+			
+		printf("\nDigite o Sobrenome para registro: ");
+		scanf("%s",sobrenome);
+			
+		file=fopen(arquivo, "a");	//atualização do arquivo com a variavel sobrenome
+		fprintf(file,sobrenome);
+		fprintf(file," , ");
+		fclose(file);
 		
-	strcpy(arquivo,cpf);
-		
-	FILE *file;
-	file=fopen(arquivo, "w");	//cria arquivo de dados, com o mesmo valor de cpf. "w" para criar, Write
-	fprintf(file,cpf);
-	fprintf(file," , ");
-	fclose(file);
-		
-	printf("\nDigite o Nome para registro: ");
-	scanf("%s",nome);
-		
-	file=fopen(arquivo, "a");	//atualização do arquivo com a variavel nome. "a" para atualizar
-	fprintf(file,nome);
-	fprintf(file," , ");
-	fclose(file);
-		
-	printf("\nDigite o Sobrenome para registro: ");
-	scanf("%s",sobrenome);
-		
-	file=fopen(arquivo, "a");	//atualização do arquivo com a variavel sobrenome
-	fprintf(file,sobrenome);
-	fprintf(file," , ");
-	fclose(file);
-	
-	printf("\nDigite o Cargo para registro: ");
-	scanf("%s",cargo);
-		
-	file=fopen(arquivo, "a");	//atualização do arquivo com a variavel cargo
-	fprintf(file,cargo);
-	fclose(file);
-		
-	system("pause");	
-	system("cls");
-		
+		printf("\nDigite o Cargo para registro: ");
+		scanf("%s",cargo);
+			
+		file=fopen(arquivo, "a");	//atualização do arquivo com a variavel cargo
+		fprintf(file,cargo);
+		fclose(file);
+			
+		printf("\n\nRealizar novo registro?		(1) para sim\n");
+		scanf("%i",&novo_registro);	
+		system("cls");
+	}
+	while(novo_registro == 1);	//Enquanto o usuário escolher o digito "1" será realizado novo registro
 }
 
 int consulta()	//Função responsavel pela consulta de usuário no banco de dados
@@ -57,66 +62,75 @@ int consulta()	//Função responsavel pela consulta de usuário no banco de dados
 	setlocale(LC_ALL, "Portuguese");
 	char cpf[14];
 	char conteudo[144];		//tamonho maximo que arquivo contando os tamanhos das variaveis do registro e os espaçamentos e mais um espaço de segurança
+	int nova_consulta = 1;
 	
-	
-	printf("|||		Consultar Usuário da EBAC		|||\n\n");
-	printf("Digite o CPF a ser consultado: ");
-	scanf("%s",cpf);
-		
-	FILE *file;
-	file = fopen(cpf,"r");	//consulta banco de dados para abrir arquivo cpf informado. "r" read, leitura
-	
-	if(file == NULL)	//caso cpf informado seja invalido informa usuário
+	do
 	{
-		printf("Operação invalida, arquivo não encontrado.\n");
-	}
-	while(fgets(conteudo, 144, file) != NULL)
-	{
-		printf("\nEssas são as informações do usuário: ");
-		printf("%s", conteudo);
-		printf("\n\n");
-	}
-	fclose(file);
+		printf("|||		Consultar Usuário da EBAC		|||\n\n");
+		printf("Digite o CPF a ser consultado: ");
+		scanf("%s",cpf);
+			
+		FILE *file;
+		file = fopen(cpf,"r");	//consulta banco de dados para abrir arquivo cpf informado. "r" read, leitura
 		
-	system("pause");	
-	system("cls");
-		
+		if(file == NULL)	//caso cpf informado seja invalido informa usuário
+		{
+			printf("Operação invalida, arquivo não encontrado.\n");
+		}
+		while(fgets(conteudo, 144, file) != NULL)
+		{
+			printf("\nEssas são as informações do usuário: ");
+			printf("%s", conteudo);
+			printf("\n\n");
+		}
+		fclose(file);
+			
+		printf("\n\nRealizar nova consulta?		(1) para sim\n");
+		scanf("%d",&nova_consulta);	
+		system("cls");
+	}
+	while(nova_consulta==1);
 }
 
 int deletar()	//Função responsavel por deletar o usuário
 {
 	setlocale(LC_ALL, "Portuguese");
 	char cpf[14];
+	int novo_del = 1;
 		
-	printf("|||		Deletar Usuário da Listagem da EBAC		|||\n\n");
-	printf("Qual o CPF do usuário que será deletado: \n");
-	scanf("%s",cpf);
-		
-	FILE *file;
-	file = fopen(cpf,"r");
-		
-	if(file == NULL)	//consulta no banco de dados o cpf informado
+	do
 	{
-		printf("O usuário não foi encontrado, insira outro CPF.\n");
-		fclose(file);
-	}
-	if(file != NULL)	//caso encontre o cpf no banco realiza o processo de deletar
-	{
-		fclose(file);	//fecha a consulta para liberar o acesso da função de deletar
-		remove(cpf);
-	
-		file = fopen(cpf,"r");	//abre novamente a consulta para verificar se arquivo foi deletado
+		printf("|||		Deletar Usuário da Listagem da EBAC		|||\n\n");
+		printf("Qual o CPF do usuário que será deletado: \n");
+		scanf("%s",cpf);
 			
-		if(file == NULL)
-		{	
-			printf("O usuário foi deletado com sucesso.\n");
+		FILE *file;
+		file = fopen(cpf,"r");
+			
+		if(file == NULL)	//consulta no banco de dados o cpf informado
+		{
+			printf("O usuário não foi encontrado, insira outro CPF.\n");
+			fclose(file);
 		}
-		fclose(file);	//encerra a consulta
-	}
+		if(file != NULL)	//caso encontre o cpf no banco realiza o processo de deletar
+		{
+			fclose(file);	//fecha a consulta para liberar o acesso da função de deletar
+			remove(cpf);
 		
-	system("pause");	
-	system("cls");
-	
+			file = fopen(cpf,"r");	//abre novamente a consulta para verificar se arquivo foi deletado
+				
+			if(file == NULL)
+			{	
+				printf("O usuário foi deletado com sucesso.\n");
+			}
+			fclose(file);	//encerra a consulta
+		}
+			
+		printf("\n\nDeseja deletar mais algum usuário?		(1) para sim\n");
+		scanf("%d",&novo_del);	
+		system("cls");
+	}
+	while(novo_del==1);
 }
 
 int main()
